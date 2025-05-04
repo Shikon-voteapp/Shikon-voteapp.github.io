13e94a9e19105d3a20109984f90db3f86e20239d
Teruyuki <mamouna.inori@outlook.jp>
2025-05-04 14:21:54 +0900

    Change Icon

--- deploy/public/flutter_bootstrap.js
+++ deploy/public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2447555269"
+    serviceWorkerVersion: "3954845036"
   }
 });
