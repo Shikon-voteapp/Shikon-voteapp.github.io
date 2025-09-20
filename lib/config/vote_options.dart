4f6befb75e4625420f0ec2377e064f7bc4e9cb3a
mamouna <mamouna,inori@outlook.jp>
2025-09-20 13:11:09 +0900

    build_20250920_v28.4.1+39

--- lib/config/vote_options.dart
+++ lib/config/vote_options.dart
@@ -85,7 +85,7 @@ class VotingPeriodConfig {
 }
 
 // データ更新日時
-final DateTime dataUpdateDate = DateTime(2025, 9, 19, 15, 59, 0); // 2025年1月15日 12:00
+final DateTime dataUpdateDate = DateTime(2025, 9, 21, 7, 4, 0); // 2025年1月15日 12:00
 
 // デフォルトの投票期間設定
 final VotingPeriodConfig defaultVotingPeriod = VotingPeriodConfig(
