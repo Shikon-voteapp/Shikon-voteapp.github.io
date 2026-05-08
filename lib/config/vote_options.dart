3e35f6c6879f770b9dc5b598214c3d3570941517
Teruyuki Ogawa <Meiji.hs.vote@gmail.com>
2026-05-08 12:48:43 +0900

    build_20260508_v1.6.7+36

--- lib/config/vote_options.dart
+++ lib/config/vote_options.dart
@@ -85,7 +85,7 @@ class VotingPeriodConfig {
 }
 
 // データ更新日時
-final DateTime dataUpdateDate = DateTime(2026, 4, 24, 5, 7, 0); // 2025年1月15日 12:00
+final DateTime dataUpdateDate = DateTime(2026, 5, 9, 3, 39, 0); // 2025年1月15日 12:00
 
 // デフォルトの投票期間設定
 final VotingPeriodConfig defaultVotingPeriod = VotingPeriodConfig(
