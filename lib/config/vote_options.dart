e686d043cbbfdcd86c88e5c37adbc9c495de200b
Teruyuki <mamouna.inori@outlook.jp>
2026-08-29 14:44:42 +0900

    build_20260829_v3.0.0+64

--- lib/config/vote_options.dart
+++ lib/config/vote_options.dart
@@ -85,7 +85,7 @@ class VotingPeriodConfig {
 }
 
 // データ更新日時
-final DateTime dataUpdateDate = DateTime(2026, 8, 29, 6, 55, 0); // 2025年1月15日 12:00
+final DateTime dataUpdateDate = DateTime(2026, 8, 30, 8, 40, 0); // 2025年1月15日 12:00
 
 // デフォルトの投票期間設定
 final VotingPeriodConfig defaultVotingPeriod = VotingPeriodConfig(
